3107af68fb215b8246ede72761638c622efe379a
Diêgo Axel Bernardo Santos Rodrigues <diegoaxelbsr@gmail.com>
2024-02-25 00:21:44 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,9 @@
-function ampliarImagem(imagenm) {
-    imagem.style.transform = "scale(1.2)"; // Ampliar a imagem 
-    imagem.style.trasition = "transform 0.5s"; // transação suave
-}
-
-function voltarImagem() {
-    const imagens = document.querySelectorAll('.img_diario');
-
-    imagens.forEach(imagem => {
-        imagem.style.transform = "scale(1)"; // voltar ao tamanho original
-        imagem.style.transitin = "transform 0.5s"; // transação suave
-    })
+function ampliarImagem(imagem) {
+    if (imagem.classList.contains('ampliada')) {
+        imagem.style.transform = "scale(1)"; // Restaura o tamanho original
+        imagem.classList.remove('ampliada');
+    } else {
+        imagem.style.transform = "scale(1.5)"; // Amplia a imagem
+        imagem.classList.add('ampliada');
+    }
 }
